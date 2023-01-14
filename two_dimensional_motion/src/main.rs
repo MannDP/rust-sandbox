d3d9e92f46cfa935b0ea105fc42a53bd62d3173c
Mann Patel <36581058+MannDP@users.noreply.github.com>
2023-01-14 01:58:51 -0500

    implement types, main boilerplate

--- two_dimensional_motion/src/main.rs
+++ two_dimensional_motion/src/main.rs
@@ -1,3 +1,4 @@
+use std::f64::consts::PI;
 use std::fmt;
 use std::ops;
 
@@ -36,32 +37,49 @@ impl fmt::Display for Point {
 }
 
 // // overloaded add operator
-// impl ops::Add<Force> for Point {
-//     type Output = Point;
+impl ops::Add<Force> for Point {
+    type Output = Self;
 
-//     fn add(self, _rhs: Force) -> Point {
-//         self
-//     }
-// }
+    fn add(self, f: Force) -> Point {
+        let mag = f.magnitude;
+        let rads = f.angle * (PI / 360f64);
+
+        let dx = mag * rads.cos();
+        let dy = mag * rads.sin();
+        Point {
+            x: self.x + dx,
+            y: self.y + dy,
+        }
+    }
+}
 
 // model a force with an angle and a magnitude
 // the angle, in degrees, start from the line out in the positive x direction of the cartesian plane, and wraps around 360 degrees
 struct Force {
     angle: f64,
-    magnitude: u32,
+    magnitude: f64,
 }
 
 impl Force {
     fn new() -> Self {
         Force {
             angle: 0f64,
-            magnitude: 0,
+            magnitude: 0f64,
         }
     }
 }
 
 // overloaded multiply operator
-// impl ops::Mul<f64> for Force {}
+impl ops::Mul<f64> for Force {
+    type Output = Self;
+
+    fn mul(self, scalar: f64) -> Force {
+        Force {
+            angle: self.angle,
+            magnitude: self.magnitude * scalar,
+        }
+    }
+}
 
 impl fmt::Display for Force {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
@@ -74,18 +92,31 @@ impl fmt::Display for Force {
 }
 
 fn main() {
-    println!("hello world");
-    // create runner loop that reads input args
-    // and print them out
+    let mut line = String::new();
+    let mut force = Force::new();
+    let mut point = Point::new();
 
-    // todo
-    // - overloaded addition operator between Point object and Force object
-    // - overloaded multiplication operator between a Force object, and a float scalar
+    // todo: how to read input stream one char at a time
+    loop {
+        let res = std::io::stdin().read_line(&mut line);
+        match res {
+            Err(_) => break,
+            _ => (),
+        }
 
-    // not doing
-    // - constructors with default args (not supported)
-    // - no overloaded input operator
+        match line.as_str() {
+            "n" => {}
+            "p" => {}
+            "+" => {}
+            "*" => {}
+            "q" => {}
+            _ => {
+                println!("Invalid test case!");
+                println!("Invalid command: '{line}'");
+                break;
+            }
+        }
+    }
 
-    let p = Point::new();
-    println!("{p}")
+    println!("exiting!")
 }
